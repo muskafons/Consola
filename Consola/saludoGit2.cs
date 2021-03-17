954af600c5ecb9f48628c730553a88755fa3fdbd
Julian <muskafons@hotmail.com>
2021-03-17 16:12:45 -0500

    creo que hizo falta el commit idiota

---
+++ Consola/saludoGit2.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Consola
+{
+    class SaludoGIT2
+    {
+        SaludoGIT()
+        {
+            Console.WriteLine("Un saludo desde GIT x 2 vecex");
+        }
+
+    }
+}
